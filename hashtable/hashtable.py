802ead9b2ac6d1bb0264e037c458c1c8fd3c26b4
unknown <you@example.com>
2020-09-29 13:42:03 -0400

    Im tired

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,11 +1,69 @@
-class HashTableEntry:
-    """
-    Linked List hash table key/value pair
-    """
-    def __init__(self, key, value):
-        self.key = key
+class Node:
+    def __init__(self, value, next = None):
         self.value = value
-        self.next = None
+        self.next = next
+
+class LinkedList:
+    def __init__(self, head = None):
+        self.head = head
+        self.size = 1
+
+    def add_to_head(self, value):
+        if self.head is None:
+            node = Node(value)
+        else:
+            prev = self.head
+            self.head = Node(value, prev)
+
+        self.size =+ 1
+
+    def remove_from_head(self):
+        if self.head is None:
+            return
+
+        current_head = self.head
+        new_head = current_head.next
+        current_head = None
+        self.head = new_head
+
+        self.size =- 1
+
+    def remove(self, value):
+        current = self.head
+        # if there is nothing to delete
+        if current is None:
+            return None
+        # when deleting head
+        if current.value[0] == value:
+            self.head = current.next
+            return current
+        # when deleting something else
+        else:
+            previous = current
+            current = current.next
+            while current is not None:
+                if current.value[0] == value: # found it!
+                    previous.next = current.next  # cut current out!
+                    return current # return our deleted node
+                else:
+                    previous = current
+                    current = current.next
+            return None # if we got here, nothing was found!
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        current_node = self.head
+        total = 0
+        str = ""
+        while current_node != None:
+            str += f"index {total}: key => {current_node.value[0]}, value => {current_node.value[1]}\n"
+            # str += f"index {total}: length {len(self)} \n"
+            current_node = current_node.next
+            total += 1
+        return str
+    # Remove from linked list
 
 
 # Hash table can't have fewer than this many slots
@@ -16,41 +74,40 @@ class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
     def __init__(self, capacity):
-        self.storage = [None] * capacity
+        # Your code here
         self.capacity = capacity
+        self.storage = [None] * self.capacity
         self.size = 0
+
 
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
         One of the tests relies on this.
-
         Implement this.
         """
         # Your code here
+        return len(self.storage)
 
 
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
         Implement this.
         """
         # Your code here
+        return self.size / self.get_num_slots()
 
 
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
-
         Implement this, and/or DJB2.
         """
 
@@ -60,14 +117,13 @@ class HashTable:
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
         Implement this, and/or FNV-1.
         """
-        # Your code here
         hash = 5381
-        for c in key:
-            hash = (hash * 33) + ord(c)
-            return hash
+        for char in key:
+            hash = (( hash << 5) + hash) + ord(char)
+        return hash
+        # Your code here
 
 
     def hash_index(self, key):
@@ -76,150 +132,137 @@ class HashTable:
         between within the storage capacity of the hash table.
         """
         #return self.fnv1(key) % self.capacity
-
-        return self.djb2(key) % self.capacity
+        return self.djb2(key) % len(self.storage)
 
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
-        index = self.hash_index(key)
-        if(self.storage[index] == None):
-            self.storage[index] = HashTableEntry(key, value)
-            self.size +=1
+        hashed_key = self.hash_index(key)
+        if self.storage[hashed_key] != None:
+            linkedList = self.storage[hashed_key]
+            linkedList.add_to_head((key, value))
         else:
-            #check if it exists 
-            curr = self.storage[index]
-            while curr.next and curr.key != key:
-                curr = curr.next
-            if curr.key == key:
-                curr.value = value
-            #it doesn't exist, so add it to the head of the list
-            else:
-                curr.next = HashTableEntry(key, value)
-                self.size +=1
-
+            self.storage[hashed_key] = LinkedList(Node((key, value)))
+        # Your code here
 
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
-        index = self.hash_index(key)
-        #node to delete was first in list
-        if self.storage[index].key == key:
-            if self.storage[index].next == None:
-                #list should now be empty
-                self.storage[index] = None
-                self.size -=1
-            #it is not the only one in the list
-            else:
-                new_head = self.storage[index].next
-                self.storage[index].next = None
-                self.storage[index] = new_head
-                self.size -=1
-        #node was not first in the list or is none
-        else:
-            if self.storage[index] == None:
-                return None
-            else:
-                curr = self.storage[index]
-                prev = None
-                #search until at end or have found key
-                while curr.next is not None and curr.key != key:
-                    prev = curr
-                    curr = curr.next
-                #found the key
-                if curr.key == key:
-                    prev.next = curr.next
-                    self.size -=1
-                    return curr.value
-                #didn't find the key
-                else:
-                    return None
+        hashed_key = self.hash_index(key)
+        if self.storage[hashed_key] == None:
+            return None
 
-        
-    
+        if len(self.storage[hashed_key]) >= 1:
+            self.storage[hashed_key].remove(key)
+        else:
+            self.storage[hashed_key].head = None
+        # Your code here
+
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
-        index = self.hash_index(key)
-        #if it is the first thing in the ll
-        if self.storage[index] is not None and self.storage[index].key == key:
-            return self.storage[index].value
-        #there's nothing there to get
-        elif self.storage[index] is None:
+        hashed_key = self.hash_index(key)
+        if self.storage[hashed_key] == None:
             return None
-        #possibly later in the ll
+
+        if len(self.storage[hashed_key]) >= 1:
+            current_head = self.storage[hashed_key].head
+
+            while current_head != None:
+                if current_head.value[0] == key:
+                    return current_head.value[1]
+                current_head = current_head.next
+            return None
         else:
-            curr = self.storage[index]
-            while curr.next != None and curr.key != key:
-                curr = self.storage[index].next
-            if curr == None:
-                return None
-            else:
-                return curr.value
-
-
-       
+            return self.storage[hashed_key].head.value[1]
+        # Your code here
 
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
         Implement this.
         """
+        self.capacity = new_capacity
+        value_key_pair = []
+        # Get all key-value from store
+        # Resize Storage
+        # Calculate hash key and insert them in resized storage
+
+        # Deep copy
+        for store in self.storage:
+            if len(store) >= 1:
+                current_head = store.head
+                while current_head != None:
+                    value_key_pair.append(current_head.value)
+                    current_head = current_head.next
+            else:
+                value_key_pair.append(store[0])
+        # print(value_key_pair[0])
+        self.storage = [None] * self.capacity
+
+        for key_value in value_key_pair:
+            self.put(key_value[0], key_value[1])
+
         # Your code here
-
-
-
 if __name__ == "__main__":
     ht = HashTable(8)
 
-    ht.put("line_1", "'Twas brillig, and the slithy toves")
-    ht.put("line_2", "Did gyre and gimble in the wabe:")
-    ht.put("line_3", "All mimsy were the borogoves,")
-    ht.put("line_4", "And the mome raths outgrabe.")
-    ht.put("line_5", '"Beware the Jabberwock, my son!')
-    ht.put("line_6", "The jaws that bite, the claws that catch!")
-    ht.put("line_7", "Beware the Jubjub bird, and shun")
-    ht.put("line_8", 'The frumious Bandersnatch!"')
-    ht.put("line_9", "He took his vorpal sword in hand;")
-    ht.put("line_10", "Long time the manxome foe he sought--")
-    ht.put("line_11", "So rested he by the Tumtum tree")
-    ht.put("line_12", "And stood awhile in thought.")
+    ht.put("key-0", "val-0")
+    ht.put("key-1", "val-1")
+    ht.put("key-2", "val-2")
+    ht.put("key-3", "val-3")
+    ht.put("key-4", "val-4")
+    ht.put("key-5", "val-5")
+    ht.put("key-6", "val-6")
+    ht.put("key-7", "val-7")
+    ht.put("key-8", "val-8")
+    ht.put("key-9", "val-9")
 
     print("")
 
     # Test storing beyond capacity
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
+    # for i in range(1, 13):
+    #     print(ht.get(f"line_{i}"))
+    # return_value = ht.get("key-8")
+    print(ht.storage[3])
+    ht.delete("key-8")
+    print(ht.storage[3])
 
+    ht.resize(16)
+
+    print(ht.storage)
+    # ht.delete("key-6")
+    # ht.delete("key-5")
+    # ht.delete("key-4")
+    # ht.delete("key-3")
+    # ht.delete("key-2")
+    # ht.delete("key-1")
+    # ht.delete("key-0")
+
+    # return_value = ht.get("key-8")
+    # print(return_value)
     # Test resizing
-    old_capacity = ht.get_num_slots()
-    ht.resize(ht.capacity * 2)
-    new_capacity = ht.get_num_slots()
+    # old_capacity = ht.get_num_slots()
+    # ht.resize(ht.capacity * 2)
+    # new_capacity = ht.get_num_slots()
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
+    # # Test if data intact after resizing
+    # for i in range(1, 13):
+    #     print(ht.get(f"line_{i}"))
 
-    print("")
+    # print("")
