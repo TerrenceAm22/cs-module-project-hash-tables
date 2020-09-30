8198249eb06f65cf20adeb1ac91f7168049ccde1
TerrenceAm22 <tamalone87@gmail.com>
2020-09-30 18:46:25 -0400

    almost there

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -3,8 +3,28 @@
 
 def expensive_seq(x, y, z):
     # Your code here
+    cache = {}
 
+    def inner_expensive_seq(x, y, z):
+        if x <= 0:
+            cache[(x, y, z)] = y + z
+            return cache[(x, y, z)]
 
+        elif (x, y, z) in cache:
+            return cache[(x, y, z)]
+
+        else:
+            cache[(x - 1, y + 1, z)] = inner_expensive_seq(x - 1, y + 1, z)
+            cache[(x - 2, y + 2, z * 2)] = inner_expensive_seq(x - 2, y + 2, z * 2)
+            cache[(x - 3, y + 3, z * 3)] = inner_expensive_seq(x - 3, y + 3, z * 3)
+
+            # return cache[(x - 1, y + 1, z)] + cache[(x - 2, y + 2, z * 2)] + cache[(x - 2, y + 2, z * 2)]
+
+            return inner_expensive_seq(x - 1, y + 1, z) + inner_expensive_seq(x - 2, y + 2, z * 2) + inner_expensive_seq(x - 3, y + 3, z * 3)
+
+    return inner_expensive_seq(x, y, z)
+    
+# print(expensive_seq(2, 2, 2))
 
 if __name__ == "__main__":
     for i in range(10):
